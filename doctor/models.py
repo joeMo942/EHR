5d13559fd89083a214dfdfe9ffb4cb7ec802ab60
joeMo942 <yousefmohamed942@gmail.com>
2024-05-23 23:16:31 +0300

    initial

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(primary_key=True, max_length=50)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    specialization = models.CharField(max_length=50, blank=True, null=True)
+    medical_id = models.IntegerField()
+    fees = models.IntegerField(blank=True, null=True)
+
+
+class Doctor(models.Model):
+    dept_name = models.ForeignKey(Department, models.DO_NOTHING, db_column='dept_name', blank=True, null=True)
+    user = models.ForeignKey('Person', on_delete=models.CASCADE)
