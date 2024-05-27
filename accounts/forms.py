8fe6abbe6eb3bd3b78cf1399629c47ebfc34ccc0
joeMo942 <yousefmohamed942@gmail.com>
2024-05-27 15:45:17 +0300

    validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,20 @@
 from django import forms
 from .models import Account
 
+
+import re
+from django.contrib.auth.password_validation import validate_password
+
+
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder':'Enter Password',
-        'class':'form-control',
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
     }))
 
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder':'Confirm Password',
-        'class':'form-control',
+        'placeholder': 'Confirm Password',
+        'class': 'form-control',
     }))
 
     birth_date = forms.DateField(widget=forms.DateInput(attrs={
@@ -17,9 +22,37 @@ class RegistrationForm(forms.ModelForm):
         'class': 'form-control',
         'type': 'date'
     }))
+
+    contact_no = forms.RegexField(
+        regex=r'^\+\d{1,3}\d{7,15}$',
+        error_messages={
+            'invalid': 'Enter a valid phone number. E.g. +123456789'
+        },
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter Contact Number with Country Code',
+            'class': 'form-control',
+        })
+    )
+
+    ssn = forms.RegexField(
+        regex=r'^\d{14}$',
+        error_messages={
+            'invalid': 'Enter a valid SSN. It should be exactly 14 digits.'
+        },
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter SSN',
+            'class': 'form-control',
+        })
+    )
+
     class Meta:
         model = Account
-        fields= ['first_name', 'last_name', 'email', 'password','type', 'ssn', 'contact_no', 'birth_date', 'gender']
+        fields = ['first_name', 'last_name', 'email', 'password', 'type', 'ssn', 'contact_no', 'birth_date', 'gender']
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        validate_password(password)  # Using Django's built-in password validators
+        return password
 
     def clean(self):
         cleaned_data = super(RegistrationForm, self).clean()
@@ -27,18 +60,13 @@ class RegistrationForm(forms.ModelForm):
         confirm_password = cleaned_data.get('confirm_password')
 
         if password != confirm_password:
-            raise forms.ValidationError(
-                "Password does not match!"
-            )
-            
+            raise forms.ValidationError("Password does not match!")
 
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
         self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
-        self.fields['ssn'].widget.attrs['placeholder'] = 'Enter SSN'
-        self.fields['contact_no'].widget.attrs['placeholder'] = 'Enter Contact Number'
 
-        for filed in self.fields:
-            self.fields[filed].widget.attrs['class'] = 'form-control form-control-lg'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control form-control-lg'
