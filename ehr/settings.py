03056a5001b64232aca4691e7edf33381d6f0f53
joeMo942 <yousefmohamed942@gmail.com>
2025-09-16 10:44:04 +0300

    testing

--- ehr/settings.py
+++ ehr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1h2kyukay1!(zsb6w0@4tc(e6%7vn5s6m)p2ai6#xlkgnmm$9x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", '.vercel.app']
 
 
 # Application definition
